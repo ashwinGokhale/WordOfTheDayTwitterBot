b68d3f6fb49405f4ec0923a59f7c4f5af9e18de9
Ashwin Gokhale <gokhale0@purdue.edu>
2017-01-07 01:03:32 -0800

    Rolled Back Random Number Testing

--- bot.js
+++ bot.js
@@ -13,14 +13,9 @@ var date = d.getFullYear() + "-" + padDigits(d.getUTCMonth() + 1, 2) + "-" +  pa
 
 var url = "http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date=" + date + "&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
 
-setInterval(sendTweets, 1000*60*60*24);
-setInterval(sendRandomNumber, 1000*20);
 
-function sendRandomNumber() {
-    T.post('statuses/update', {status: Math.floor(Math.random() * 100)}, function (err, data, response) {
-        console.log(data);
-    });
-}
+sendTweets();
+//setInterval(sendTweets, 1000*60*60*24);
 
 function sendTweets() {
     console.log("About to tweet something");
