06e6a462e3cffab0575c43d6507dbe9da2fdc46e
Ashwin Gokhale <gokhale0@purdue.edu>
2017-01-07 00:52:44 -0800

    Initial Commit

---
+++ bot.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Ashwin on 1/6/2017.
+ */
+var Twit = require('twit');
+var config = require('./config');
+var request = require("request");
+
+
+var T = new Twit(config);
+
+var d = new Date();
+var date = d.getFullYear() + "-" + padDigits(d.getUTCMonth() + 1, 2) + "-" +  padDigits(d.getDay() + 1, 2);
+
+var url = "http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date=" + date + "&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+
+setInterval(sendTweets, 1000*60*60*24);
+
+function sendTweets() {
+    console.log("About to tweet something");
+    request({
+        url: url,
+        json: true
+    }, function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            var text = (date + ": \"" + body.word + "\" (" + body.definitions[0].partOfSpeech +  ") - " + body.definitions[0].text); // Print the json response
+            tweetSomething(text);
+            console.log("Just tweeted:\n", text);
+        }
+    });
+}
+
+function tweetSomething(text) {
+    var tweet = {
+        status: text
+    };
+
+    var tweeted = function (err, data, response) {
+        console.log(data);
+    };
+
+    T.post('statuses/update', tweet, tweeted);
+}
+
+function padDigits(number, digits) {
+    return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
+}
---
+++ config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Ashwin on 1/6/2017.
+ */
+
+module.exports = {
+    consumer_key:         '5b1RGsfmljoaoIr3kywC1Mdde',
+    consumer_secret:      'LQB0szxg9qqJ24iyJkXthuNwG5hQr9wCO40vueiOkKdoU8dwie',
+    access_token:         '817642973819125760-SeertG9oMmI4qRNeVbf2d6fPy1JHqMM',
+    access_token_secret:  'DQuJitMbwQNhGGut1ceT1XPNLqEEX43sqYqlTjjNwnpQE',
+    timeout_ms:           60*1000  // optional HTTP request timeout to apply to all requests.
+};
