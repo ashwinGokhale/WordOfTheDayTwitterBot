a09dcd5f12b5d76b96cb2da212409d6810d7815c
Ashwin Gokhale <gokhale0@purdue.edu>
2017-01-08 01:52:32 -0800

    Testing

--- bot.js
+++ bot.js
@@ -13,7 +13,9 @@ var date = d.getFullYear() + "-" + padDigits(d.getUTCMonth() + 1, 2) + "-" +  pa
 
 var url = "http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date=" + date + "&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
 
-setInterval(sendTweets, 1000*60*60*24);
+
+tweetSomething("This is a test");
+//setInterval(sendTweets, 1000*60*60*24);
 
 function sendTweets() {
     console.log("About to tweet something");
--- config.js
+++ config.js
@@ -3,9 +3,9 @@
  */
 
 module.exports = {
-    consumer_key:         '5b1RGsfmljoaoIr3kywC1Mdde',
-    consumer_secret:      'LQB0szxg9qqJ24iyJkXthuNwG5hQr9wCO40vueiOkKdoU8dwie',
-    access_token:         '817642973819125760-SeertG9oMmI4qRNeVbf2d6fPy1JHqMM',
-    access_token_secret:  'DQuJitMbwQNhGGut1ceT1XPNLqEEX43sqYqlTjjNwnpQE',
+    consumer_key:         process.env.consumer_key,
+    consumer_secret:      process.env.consumer_secret,
+    access_token:         process.env.access_token,
+    access_token_secret:  process.env.access_token_secret,
     timeout_ms:           60*1000  // optional HTTP request timeout to apply to all requests.
 };
