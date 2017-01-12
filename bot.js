013dad97513a2289e516e0e15f4951e901e13b2b
Ashwin Gokhale <gokhale0@purdue.edu>
2017-01-12 17:03:07 -0500

    Checking if environment variables are properly set

--- bot.js
+++ bot.js
@@ -2,10 +2,11 @@
  * Created by Ashwin on 1/6/2017.
  */
 
-process.env['access_token'] = "817642973819125760-SeertG9oMmI4qRNeVbf2d6fPy1JHqMM";
-process.env['access_token_secret'] = "DQuJitMbwQNhGGut1ceT1XPNLqEEX43sqYqlTjjNwnpQE";
-process.env['consumer_key'] = "5b1RGsfmljoaoIr3kywC1Mdde";
-process.env['consumer_secret'] = "LQB0szxg9qqJ24iyJkXthuNwG5hQr9wCO40vueiOkKdoU8dwie";
+// process.env['access_token'] = "817642973819125760-SeertG9oMmI4qRNeVbf2d6fPy1JHqMM";
+// process.env['access_token_secret'] = "DQuJitMbwQNhGGut1ceT1XPNLqEEX43sqYqlTjjNwnpQE";
+// process.env['consumer_key'] = "5b1RGsfmljoaoIr3kywC1Mdde";
+// process.env['consumer_secret'] = "LQB0szxg9qqJ24iyJkXthuNwG5hQr9wCO40vueiOkKdoU8dwie";
+// process.env['access_key'] = "e3d33762e4a0e893916110068f50f29100793a317a2c6b40f";
 
 // Import: Twit, API keys, request
 var Twit = require('twit');
@@ -16,7 +17,8 @@ var schedule = require('node-schedule');
 // Authenticate API keys and create new Twit object
 var T = new Twit(config);
 
-setInterval(sendTweets, 1000*60*60*24);
+//setInterval(sendTweets, 1000*60*60*24);
+tweetSomething("Everything is all good");
 
 
 function sendTweets() {
@@ -26,7 +28,7 @@ function sendTweets() {
     var date = d.getFullYear() + "-" + padDigits(d.getUTCMonth() + 1, 2) + "-" +  padDigits(d.getDate(), 2);
 
     // Insert new date into the url
-    var url = "http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date=" + date + "&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+    var url = "http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date=" + date + "&api_key=" + process.env.access_key;
 
 
     // Make a request to the Wordnik API for word of the day
@@ -44,32 +46,66 @@ function sendTweets() {
 }
 
 function tweetSomething(text) {
+    var textArr = text.trim().split(" ");
+    var tweetTemp = [];
+    var toTweet = [];
+    var count = 0;
+    while (textArr.length > 0){
+        var word = textArr[0];
+        if (count + word.length + 1 < 140){
+            count += word.length + 1;
+            textArr.shift();
+            tweetTemp.push(word);
+        }
+        else{
+            toTweet.push(tweetTemp.join(" "));
+            count = 16; // Length of @WordOfTheDayRobotUSA
+            tweetTemp = [];
+        }
+    }
+
+    if(tweetTemp.length > 0)
+        toTweet.push(tweetTemp.join(" "));
+
     var tweet = {
-        status: text
+        status: toTweet[0]
     };
 
+    toTweet.shift();
+
+
     // If tweet exceeds the 140 character limit, split the tweet and finish it in a reply chain
-    if (tweet.status.length > 140){
-        var otherHalf = " " + tweet.status.substring(140);
-        tweet.status = tweet.status.substring(0,140);
-
+    if (toTweet.length >= 1){
+        // Send initial tweet
         T.post('statuses/update', tweet, function (err, data, response) {
-            console.log("Just sent tweet:\n" + tweet);
+            console.log("Just sent tweet:\n" + JSON.stringify(tweet));
+            console.log("Error: " + err);
+            console.log("toTweet length: " + toTweet.length);
 
-             var replyTweet = {
-                status: "@" + data.user.screen_name + otherHalf,
+            // Start reply chain
+            tweet = {
+                status: "@WordOfTheDayUSA " + toTweet[0],
                 in_reply_to_status_id: data.id_str
             };
 
-            T.post('statuses/update', replyTweet, function (err, data, response) {
-                console.log("Just replied with:\n" + replyTweet);
+            T.post('statuses/update', tweet, function (err, data, response) {
+                console.log("Just sent reply:\n" + JSON.stringify(tweet));
+                console.log("Error: " + err);
+
+                toTweet.shift();
+                console.log("toTweet length: " + toTweet.length);
             });
+
         });
+
+
     }
 
-    else {
+    // Otherwise send tweet are usual
+     else {
         T.post('statuses/update', tweet, function (err, data, response) {
             console.log("Just sent tweet:\n" + tweet);
+            console.log("Error: " + err);
         });
     }
 }
